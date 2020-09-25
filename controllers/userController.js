0b8ea8e22debebdadcb3cdcfc86f4ad9dd3bb7df
han-hyerin <68931265+han-hyerin@users.noreply.github.com>
2020-09-25 21:00:18 +0900

    200925

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,21 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Log In");
-export const logout = (req, res) => res.render("Log Out");
-export const users = (req, res) => res.render("Users");
-export const userDetail = (req, res) => res.render("User Detail");
-export const editProfile = (req, res) => res.render("Edit Profile");
-export const changePassword = (req, res) => res.render("Change Password");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("Join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  res.render("Join", { pageTitle: "Join" });
+};
+
+export const login = (req, res) =>
+  res.render("Log In", { pageTitle: "Log In" });
+export const logout = (req, res) =>
+  res.render("Log Out", { pageTitle: "Log Out" });
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("User Detail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("Edit Profile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("Change Password", { pageTitle: "Change Password" });
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 1111,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 11111,
+    title: "Video 2",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 111111,
+    title: "Video 3",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 1111111,
+    title: "Video 4",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/details/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
