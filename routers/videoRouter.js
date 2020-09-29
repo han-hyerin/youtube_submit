2f712fcac2fd7201d80f1c189dcfd1dae93210bc
han-hyerin <68931265+han-hyerin@users.noreply.github.com>
2020-09-29 16:15:36 +0900

    w/ mongodb

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,17 +1,17 @@
 import express from "express";
+import routes from "../routes";
 import {
-  deleteVideo,
-  editVideo,
   getUpload,
   postUpload,
   videoDetail,
-  videos,
+  editVideo,
+  deleteVideo,
 } from "../controllers/videoController";
-import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
